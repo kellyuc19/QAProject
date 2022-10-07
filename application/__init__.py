672f00209f7e8cc3808585846b1ce67dcf32d473
Paul Mercer <paul.mercer@qa.com>
2022-10-07 10:07:00 +0100

    Get db connection from env variable

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -10,10 +11,9 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@123.45.6.78:3306/mydatabase'
 app.config['SECRET_KEY'] = 'bA5qzruPYLAyyx5QFNUVCg'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
 
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
